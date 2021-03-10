eeed4f33aa7496eb56e3b92b6a6ab6aefe28fddf
Metin-Trampov <63451415+MetinTrampov@users.noreply.github.com>
2021-03-10 20:28:58 +0200

    Add files via upload

--- Dwarf.java
+++ Dwarf.java
@@ -1,6 +1,27 @@
 package Piace;
 
-public class Dwarf {
+import java.awt.*;
 
+public class Dwarf extends Piace {
 
+    public Dwarf(int row, int col, Color color) {
+
+        this.row    = row;
+        this.col    = col;
+        this.color  = color;
+        this.id     = "D";
+    }
+
+    public boolean isMoveValid(int moveRow, int moveCol) {
+
+        int rowCoeficient = Math.abs(moveRow - this.row);
+        int colCoeficient = moveCol - this.col;
+
+        return  rowCoeficient == 1 &&
+                colCoeficient == 0;
+    }
+
+    public boolean isAttackValid(int attackRow, int attackCol) {
+        return false;
+    }
 }
